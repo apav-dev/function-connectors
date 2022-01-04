41a60192aff11880de7a6102d50c7db22ef857e1
Aaron Pavlick <apavlick@yext.com>
2022-01-04 11:23:19 -0500

    updated fetchMovies test

--- fetch-movies/test.ts
+++ fetch-movies/test.ts
@@ -1,16 +1,31 @@
 import fetchMovies from './mod.ts';
+import { assertEquals, assertExists } from "https://deno.land/std@0.114.0/testing/asserts.ts";
 
 Deno.test("fetchMovies", async () => {
-  const stateStringObj= { pageToken: '' };
+  const nextPageToken = { pageToken: '' };
+  const movies = [];
 
   do {
-    const responseString = await fetchMovies(JSON.stringify(stateStringObj));
+    const responseString = await fetchMovies(JSON.stringify(nextPageToken));
     const responseObj = JSON.parse(responseString);
 
-    console.log("Movie Details: " + JSON.stringify(responseObj.data));
+    const movie = responseObj.data;
+
+    console.log("Movie Details: " + JSON.stringify(movie));
     console.log("Next Page Token: " + responseObj.nextPageToken);
 
-    stateStringObj.pageToken = responseObj.nextPageToken;
+    // assert that response contains details from Get Movie Details and Get Movie Images APIs
+    assertExists(movie);
+    assertExists(movie.imdb_id);
+    assertExists(movie.title);
+    assertExists(movie.poster);
 
-  } while (JSON.parse(stateStringObj.pageToken).nextPage < 3) 
+    movies.push(movie);
+
+    nextPageToken.pageToken = responseObj.nextPageToken;
+
+  } while (JSON.parse(nextPageToken.pageToken).nextPage === 2);
+
+  // assert the 20 movies from first page were fetched along with 1st movie from page 2
+  assertEquals(21, movies.length);
 })
